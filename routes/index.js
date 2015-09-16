e8b1af5dc9f60cb4c1f73887c13d3c6d9e3bad20
heartbeatsfeeling <499954367@qq.com>
2015-09-16 17:35:44 +0800

    add commit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,3 @@
 module.exports=function(app){
-	require('./register.js')(app);
-	require('./login.js')(app);
-	require('./edit.js')(app);
-	require('./success.js')(app);
-	require('./view.js')(app);
-	require('./ajax.js')(app);
+	//require('./register.js')(app);
 };
