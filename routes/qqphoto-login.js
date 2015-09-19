f8e13c6e8be32d9ef877968ccb3ab6f5607ffd18
heartbeatsfeeling <499954367@qq.com>
2015-09-19 23:35:14 +0800

    add commit

--- routes/qqphoto-login.js
+++ routes/qqphoto-login.js
@@ -1,5 +1,66 @@
-module.exports = function(app) {//登录
-	app.get("/admin",function(req,res){//管理员
+module.exports = function(app) { //登录
+	var crypto = require('crypto');
+	var mongodb = require('mongodb');
+	var MongoClient = mongodb.MongoClient;
+	var db;
+	MongoClient.connect('mongodb://115.28.213.244:27017/qqphoto', function(err, database) {
+		if (err) {
+			console.log(err);
+		} else {
+			db = database;
+			console.log("Listening on port 3000");
+		};
+	});
+	app.get("/admin", function(req, res) { //管理员
 		res.render('login');
 	});
+	app.post("/admin", function(req, res) {
+		var body = req.body,
+			userName = body.userName,
+			password = body.password,
+			result={
+				code:0,
+				msg:""
+			},
+			md5,
+			md5Password='';
+		if(!userName||!password){
+			result={
+				code:0,
+				msg:"用户名或密码不能为空"
+			};
+			res.json(result)	
+		}else{
+			md5=crypto.createHash('md5');
+			md5.update(password);
+			md5Password=md5.digest('hex');
+			console.log(md5Password)
+			result.md5Password=md5Password;
+			db.collection('user').find({
+				name:userName,
+				password:md5Password
+			}).toArray(function(error,data){
+				if(error){
+					result={
+						code:0,
+						msg:"查询数据库失败"
+					}
+				}else{
+					if(data.length){
+						result={
+							code:1,
+							msg:"登录成功"
+						}
+					}else{
+						result={
+							code:0,
+							msg:"用户不存在或密码错误"
+						}
+					}
+				};
+				res.json(result)	
+			})
+		}
+		
+	})
 };
--- static/js/login.js
+++ static/js/login.js
@@ -1,13 +1,36 @@
 require(['config'], function(config) {
 	var r = require.config(config.requireConfig);
 	r(['jquery', 'jquery.validate'], function() {
-		var $form=$('form');
-		$form.bind('submit',function(e){
+		var $form = $('form');
+		var $button = $("button")
+		$form.bind('submit', function(e) {
 			e.preventDefault();
-			if(validate.get('form').valid()){
+			if (validate.get('form').valid()) {
+				if ($button.hasClass('loadingStatus')) {
+					return false;
+				} else {
+					$button.html("<i class='loading2'></i>").addClass('loadingStatus');
+					$.ajax({
+						url: $form.attr('action'),
+						type: $form.attr('method'),
+						data: $form.serialize(),
+						dataType:"json",
+						success: function(data) {
+							if(data.code==0){
+								console.log(data.msg)
+								$('.tip').html(data.msg)
+							}else{
+								$('.tip').html("")
+							}
+						},
+						error: function() {},
+						complete: function() {
+							$button.html("登录").removeClass('loadingStatus')
+						}
+					})
+				}
+			} else {
 
-			}else{
-				
 			}
 		});
 		validate({
@@ -16,18 +39,18 @@ require(['config'], function(config) {
 				$('.tip').html(tip);
 			},
 			rules: {
-				'userName': { 
-					wrong: "请输入用户名和密码", 
-					right: "", 
-					empty: '请输入用户名和密码', 
-					focus: "", 
+				'userName': {
+					wrong: "请输入用户名和密码",
+					right: "",
+					empty: '请输入用户名和密码',
+					focus: "",
 					required: true
 				},
 				'password': {
-					wrong: "请输入用户名和密码", 
-					right: "", 
-					empty: '请输入用户名和密码', 
-					focus: "", 
+					wrong: "请输入用户名和密码",
+					right: "",
+					empty: '请输入用户名和密码',
+					focus: "",
 					required: true
 				}
 			}
